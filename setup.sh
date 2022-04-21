f8681dab0ff0625ba13d256342974429fe23b042
Gabriel Brand <gabr.brand@gmail.com>
2022-04-21 20:39:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,7 +59,7 @@ sudo dnf -y install google-chrome-stable
 #Install GitHub CLI (rpm)
 sudo dnf install 'dnf-command(config-manager)'
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf install gh
+sudo dnf -y install gh
 gh auth login
 
 #Install Clapper (flatpak)
