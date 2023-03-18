0731ef5bf64fe45b0b5ef55a8d3e8c63a48da0c9
Gabriel Brand <gabr.brand@gmail.com>
2023-03-18 22:08:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # |  _|  __/ (_| | (_) | | | (_| |  ___) |  __/ |_| |_| | |_) |
 # |_|  \___|\__,_|\___/|_|  \__,_| |____/ \___|\__|\__,_| .__/
 #                                                       |_|
-# A setup script for Fedora (37)
+# A setup script for Fedora (38)
 # https://github.com/gabrbrand/fedora-setup
 # -> 7 Things You MUST DO After Installing Fedora 36 - TechHut (https://youtu.be/RrRpXs2pkzg)
 #
@@ -16,160 +16,162 @@
 
 #!/bin/bash
 
-# Optimize DNF Config
-echo 'fastestmirror=True
+sudo dnf -y install lolcat &>> ~/setup.log
+
+printf "\n [1/35] Optimize DNF Config...\n" | lolcat
+echo "fastestmirror=True
 max_parallel_downloads=10
 defaultyes=True
-keepcache=True' | sudo tee -a /etc/dnf/dnf.conf
+keepcache=True" | sudo tee -a /etc/dnf/dnf.conf &>> ~/setup.log
 
-# Install Updates
-sudo dnf -y upgrade
+printf " [2/35] Install Updates...\n" | lolcat
+sudo dnf -y upgrade &>> ~/setup.log
 
-# Enable the RPM Fusion repositories
+printf " [3/35] Enable the RPM Fusion repositories...\n" | lolcat
 sudo dnf -y install \
-  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm &>> ~/setup.log
 sudo dnf -y install \
-  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &>> ~/setup.log
 
-# Install plugins for playing movies and music
-sudo dnf -y install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-sudo dnf -y install lame\* --exclude=lame-devel
-sudo dnf -y group upgrade --with-optional Multimedia
+printf " [4/35] Install plugins for playing movies and music...\n" | lolcat
+sudo dnf -y install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel &>> ~/setup.log
+sudo dnf -y install lame\* --exclude=lame-devel &>> ~/setup.log
+sudo dnf -y group upgrade --with-optional Multimedia &>> ~/setup.log
 
-# Change Hostname
-sudo hostnamectl set-hostname 'desktop-gabriel'
+printf " [5/35] Change Hostname...\n" | lolcat
+sudo hostnamectl set-hostname 'desktop-gabriel' &>> ~/setup.log
 
-# Remove unused applications
-sudo dnf -y remove cheese gnome-connections gnome-maps gnome-photos gnome-tour rhythmbox totem
+printf " [6/35] Remove unused applications...\n" | lolcat
+sudo dnf -y remove cheese gnome-connections gnome-maps gnome-photos gnome-tour rhythmbox totem &>> ~/setup.log
 
-# Remove unused GNOME Shell Extensions
-sudo dnf -y remove gnome-classic-session gnome-shell-extension-background-logo
+printf " [7/35] Remove unused GNOME Shell Extensions...\n" | lolcat
+sudo dnf -y remove gnome-classic-session gnome-shell-extension-background-logo &>> ~/setup.log
 
-# Add Flathub remote
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+printf " [8/35] Add Flathub remote...\n" | lolcat
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &>> ~/setup.log
 
-# Enable Flathub remote
-flatpak remote-modify --enable flathub
+printf " [9/35] Enable Flathub remote...\n" | lolcat
+flatpak remote-modify --enable flathub &>> ~/setup.log
 
-# Install flatpak applications
-flatpak -y install flathub app.drey.Dialect com.github.marktext.marktext com.github.rafostar.Clapper com.github.unrud.VideoDownloader com.lakoliu.Furtherance com.mattjakeman.ExtensionManager com.spotify.Client io.bassi.Amberol io.github.realmazharhussain.GdmSettings me.dusansimic.DynamicWallpaper net.poedit.Poedit org.bluej.BlueJ org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark org.mozilla.Thunderbird
+printf " [10/35] Install flatpak applications...\n" | lolcat
+flatpak -y install flathub app.drey.Dialect com.github.marktext.marktext com.github.rafostar.Clapper com.github.unrud.VideoDownloader com.lakoliu.Furtherance com.mattjakeman.ExtensionManager com.spotify.Client io.bassi.Amberol io.github.realmazharhussain.GdmSettings me.dusansimic.DynamicWallpaper net.poedit.Poedit org.bluej.BlueJ org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark org.mozilla.Thunderbird &>> ~/setup.log
 
-# Enable adw-gtk3 copr repo
-sudo dnf -y copr enable nickavem/adw-gtk3
+printf " [11/35] Enable adw-gtk3 copr repo...\n" | lolcat
+sudo dnf -y copr enable nickavem/adw-gtk3 &>> ~/setup.log
 
-# Add GitHub CLI repo
-sudo dnf install 'dnf-command(config-manager)'
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+printf " [12/35] Add GitHub CLI repo...\n" | lolcat
+sudo dnf install 'dnf-command(config-manager)' &>> ~/setup.log
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo &>> ~/setup.log
 
-# Install Third Party Repositories
-sudo dnf -y install fedora-workstation-repositories
+printf " [13/35] Install Third Party Repositories...\n" | lolcat
+sudo dnf -y install fedora-workstation-repositories &>> ~/setup.log
 
-# Enable the Google Chrome repo
-sudo dnf -y config-manager --set-enabled google-chrome
+printf " [14/35] Enable the Google Chrome repo...\n" | lolcat
+sudo dnf -y config-manager --set-enabled google-chrome &>> ~/setup.log
 
-# Enable lazygit copr repo
-sudo dnf -y copr enable atim/lazygit
+printf " [15/35] Enable lazygit copr repo...\n" | lolcat
+sudo dnf -y copr enable atim/lazygit &>> ~/setup.log
 
-# Install rpm applications
-sudo dnf -y install adw-gtk3 bat gh gimp gnome-tweaks google-chrome-stable grub-customizer keepassxc lazygit lsd neofetch plymouth-plugin-script util-linux-user vim zsh
+printf " [16/35] Install rpm applications...\n" | lolcat
+sudo dnf -y install adw-gtk3 bat gh gimp gnome-tweaks google-chrome-stable grub-customizer keepassxc lazygit lsd neofetch plymouth-plugin-script util-linux-user vim zsh zstd &>> ~/setup.log
 
-# Install Anki
+printf " [17/35] Install Anki...\n" | lolcat
 ANKI_VERSION=$(curl --silent "https://api.github.com/repos/ankitects/anki/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-wget -P ~ https://github.com/ankitects/anki/releases/download/$ANKI_VERSION/anki-$ANKI_VERSION-linux-qt6.tar.zst
-tar xaf ~/anki-$ANKI_VERSION-linux-qt6.tar.zst
+wget -P ~ https://github.com/ankitects/anki/releases/download/$ANKI_VERSION/anki-$ANKI_VERSION-linux-qt6.tar.zst &>> ~/setup.log
+tar xaf ~/anki-$ANKI_VERSION-linux-qt6.tar.zst &>> ~/setup.log
 cd ~/anki-$ANKI_VERSION-linux-qt6
-sudo ./install.sh
+sudo ./install.sh &>> ~/setup.log
 cd ~
 rm -rf ~/anki-$ANKI_VERSION-linux-qt6
 rm ~/anki-$ANKI_VERSION-linux-qt6.tar.zst
 
-# Install chezmoi
+printf " [18/35] Install chezmoi...\n" | lolcat
 CHEZMOI_GITHUB_RELEASE_VERSION=$(curl --silent "https://api.github.com/repos/twpayne/chezmoi/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 CHEZMOI_VERSION=$(echo $CHEZMOI_GITHUB_RELEASE_VERSION | sed 's/v//')
-wget -P ~ https://github.com/twpayne/chezmoi/releases/download/$CHEZMOI_GITHUB_RELEASE_VERSION/chezmoi-$CHEZMOI_VERSION-x86_64.rpm
-sudo rpm -ivh ~/chezmoi-$CHEZMOI_VERSION-x86_64.rpm
+wget -P ~ https://github.com/twpayne/chezmoi/releases/download/$CHEZMOI_GITHUB_RELEASE_VERSION/chezmoi-$CHEZMOI_VERSION-x86_64.rpm &>> ~/setup.log
+sudo rpm -ivh ~/chezmoi-$CHEZMOI_VERSION-x86_64.rpm &>> ~/setup.log
 rm ~/chezmoi-$CHEZMOI_VERSION-x86_64.rpm
 
-# Install Printer and Scanner Drivers (MFC-9142CDN)
+printf " [19/35] Install Printer and Scanner Drivers (MFC-9142CDN)...\n" | lolcat
 mkdir ~/linux-brprinter
-wget -P ~/linux-brprinter https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz
+wget -P ~/linux-brprinter https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz &>> ~/setup.log
 cd ~/linux-brprinter
-gunzip linux-brprinter-installer-2.2.3-1.gz
+gunzip linux-brprinter-installer-2.2.3-1.gz &>> ~/setup.log
 sudo bash linux-brprinter-installer-2.2.3-1 MFC-9142CDN
 cd ~
 rm -rf ~/linux-brprinter
 
-# Install Tutanota
+printf " [20/35] Install Tutanota...\n" | lolcat
 mkdir ~/.Applications
-wget -P ~/.Applications https://mail.tutanota.com/desktop/tutanota-desktop-linux.AppImage
+wget -P ~/.Applications https://mail.tutanota.com/desktop/tutanota-desktop-linux.AppImage &>> ~/setup.log
 chmod +x ~/.Applications/tutanota-desktop-linux.AppImage
 
-# Set Plymouth Theme
+printf " [21/35] Set Plymouth Theme...\n" | lolcat
 cd ~
-git clone https://github.com/adi1090x/plymouth-themes.git
+git clone https://github.com/adi1090x/plymouth-themes.git &>> ~/setup.log
 sudo cp -r ~/plymouth-themes/pack_3/hud_space/ /usr/share/plymouth/themes/
 sudo plymouth-set-default-theme -R hud_space
 rm -rf ~/plymouth-themes
 
-# Install Fluent icon theme
+printf " [22/35] Install Fluent icon theme...\n" | lolcat
 cd ~
-git clone https://github.com/vinceliuice/Fluent-icon-theme.git
+git clone https://github.com/vinceliuice/Fluent-icon-theme.git &>> ~/setup.log
 cd ~/Fluent-icon-theme
-./install.sh -r
+./install.sh -r &>> ~/setup.log
 cd ~
-rm -rf ~/Colloid-icon-theme
+rm -rf ~/Fluent-icon-theme
 
-# Download background
+printf " [23/35] Download background...\n" | lolcat
 cd ~/.config
-wget -O background https://unsplash.com/photos/WMPmZN_1VE8/download\?ixid\=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjc2MTM5MTUw\&force\=true
+wget -O background https://unsplash.com/photos/W3kxCizLWvo/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjc5MTY4NzMy&force=true &>> ~/setup.log
 cd ~
 
-# Center new windows, set themes + background
+printf " [24/35] Center new windows, set themes + background...\n" | lolcat
 gsettings set org.gnome.mutter center-new-windows true
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Fluent-dark'
 gsettings set org.gnome.desktop.background picture-uri file:///home/gabriel/.config/background
 gsettings set org.gnome.desktop.background picture-uri-dark file:///home/gabriel/.config/background
 
-# Add dnf aliases
-sudo dnf alias add in=install
-sudo dnf alias add rm=remove
-sudo dnf alias add if=info
-sudo dnf alias add se=search
+printf " [25/35] Add dnf aliases...\n" | lolcat
+sudo dnf alias add in=install &>> ~/setup.log
+sudo dnf alias add rm=remove &>> ~/setup.log
+sudo dnf alias add if=info &>> ~/setup.log
+sudo dnf alias add se=search &>> ~/setup.log
 
-# Show Password Asterisks in Terminal
+printf " [26/35] Show Password Asterisks in Terminal...\n" | lolcat
 sudo sed -i 's/Defaults    env_reset/Defaults    env_reset,pwfeedback/' /etc/sudoers
 
-# Git Configuration
+printf " [27/35] Git Configuration...\n" | lolcat
 git config --global user.name 'Gabriel Brand'
 git config --global user.email gabr.brand@gmail.com
 git config --global init.defaultBranch main
 
-# Change shell to zsh
+printf " [28/35] Change shell to zsh...\n" | lolcat
 chsh -s $(which zsh)
 
-# Install Oh My Zsh!
+printf " [29/35] Install Oh My Zsh!...\n" | lolcat
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+printf " [30/35] Install zsh-autosuggestions...\n" | lolcat
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &>> ~/setup.log
 
-# Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+printf " [31/35] Install zsh-syntax-highlighting...\n" | lolcat
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &>> ~/setup.log
 
-# Install MesloLGS NF
+printf " [32/35] Install MesloLGS NF...\n" | lolcat
 mkdir -p ~/.local/share/fonts
-wget -P ~/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-fc-cache -f -v
+wget -P ~/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf &>> ~/setup.log
+fc-cache -f -v &>> ~/setup.log
 
-# Install Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+printf " [33/35] Install Powerlevel10k...\n" | lolcat
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &>> ~/setup.log
 
-# Apply dotfiles
+printf " [34/35] Apply dotfiles...\n" | lolcat
 GITHUB_USERNAME='gabrbrand'
-chezmoi init --apply $GITHUB_USERNAME
+chezmoi init --apply $GITHUB_USERNAME &>> ~/setup.log
 
-# Set keyboard shortcuts
+printf " [35/35] Set keyboard shortcuts...\n" | lolcat
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
@@ -179,3 +181,13 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Dateien'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'nautilus'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>E'
+
+printf "\n Complete!\n\n" | lolcat
+
+for s in {60..1}
+do
+    echo -ne " The system will restart automatically in $s seconds! \r" | lolcat
+    sleep 1
+done
+echo -ne " The system restarts now!                           \n\n" | lolcat
+reboot
